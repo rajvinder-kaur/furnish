212255cea7bd852a6d13e5a2fe5d1b5032c24915
rajvinder-kaur <kaurrajvinder736@gmail.com>
2023-08-28 19:23:29 +0530

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import {Foot} from '../Styles/Footer'
-
+import React from 'react';
+import { Foot } from '../Styles/Footer';
+import { Link } from 'react-router-dom';
 function Footer() {
-  return (
-    <Foot>
-
-    </Foot>
-  )
+    return (
+        <Foot>
+            <td><h1>Furnish</h1></td>
+            <td>We turn your furniture dreams into reality with a personalized touch that will make you fall in love with your space all over again.</td>
+            <td>
+            </td>
+            <td> <h3>Reach Us</h3> </td>
+            <td>
+                <li>Milap Nagar ,Ambala city</li>
+                <li>+9124******</li>
+                <li>furnish@gmail.com</li>
+            </td>
+            <td>Made by Rajvinder Kaur</td>
+        </Foot>
+    )
 }
 
 export default Footer
--- src/Styles/Footer.js
+++ src/Styles/Footer.js
@@ -3,4 +3,35 @@ import {Parent} from './Common'
 
 export const Foot = styled(Parent)`
 background: #FDFBF7;
+display:grid;
+margin:auto;
+padding-left:300px;
+padding-right:200px;
+padding-top:100px;
+padding-bottom:100px
+grid-template-columns: 1fr 1fr ;
+grid-gap:20px;
+column-gap:200px;
+text-align:left;
+h1{
+    
+}
+td{
+    text-align:left;
+
+    &:nth-child(2){
+        grid-row:2;
+        grid-column:1;
+        width:600px
+    }
+    &:nth-child(4){
+        grid-row:1;
+        grid-column:2;
+    }
+    &:nth-child(5){
+        grid-row:2;
+        grid-column:2;
+        list-style-type:none;
+    }
+}
 `
